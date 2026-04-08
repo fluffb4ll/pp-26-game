1da22ecb73c5f120d4c6c529d78ae8306f5c25c6
fluffb4ll <real.fluffball@gmail.com>
2026-04-08 21:29:04 +0500

    Implemented cloud save system

---
+++ Assets/Scripts/SDK/Saves.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace YG
+{
+    /// <summary>
+    /// Содержит параметры, которые будут сохранятся в облаке.
+    /// Доступ к ним можно получить, используя <c>YG.saves.[paramName]</c>
+    /// </summary>
+    public partial class SavesYG
+    {
+        public int coins = 5;   // TODO: разобраться, нужно ли инициализировать со значением
+    }
+}
+
