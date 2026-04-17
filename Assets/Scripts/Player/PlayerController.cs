497fb752f2bd41d87fd347621b65db5dc1ace21c
Ivan Antropov <real.fluffball@gmail.com>
2026-04-18 03:27:13 +0500

    Added simple damage, death and respawn logic to player
    Also added a simple death animation - player will now fall on his back when he dies.
    There is a DamageZone trigger that can be used to test damage input. It should be removed later.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,86 @@
+using System;
+using Interfaces;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+using UnityEngine.TextCore.Text;
+
+namespace Player
+{
+    public class PlayerController : MonoBehaviour, IDamageable
+    {
+        [SerializeField] private PlayerInteraction playerInteraction;
+        [SerializeField] private PlayerMovement playerMovement;
+        [SerializeField] private Transform spawnPoint;
+        [SerializeField] private CharacterController charController;
+        [SerializeField] private float deathAnimationSpeed;
+        
+        [SerializeField] private InputActionReference respawnBindings;
+        
+        public int maxHealth;
+        public int damage;
+        public int health;
+
+        private bool _isDying = false;
+        private InputAction _respawnAction;
+        
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            _respawnAction = respawnBindings.action;
+            health = maxHealth;
+        }
+        
+        // Update is called once per frame
+        void Update()
+        {
+            if (_isDying && transform.rotation.x > -0.5f)
+                PlayDeathAnimation(deathAnimationSpeed);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("DamageZone"))
+                Die();
+        }
+        
+        private void OnRespawn(InputAction.CallbackContext context)
+        {
+            _respawnAction.performed -= OnRespawn;
+            Respawn();
+        }
+        
+        public void TakeDamage(int damageAmount)
+        {
+            health -= damageAmount;
+            
+            if (health <= 0)
+                Die();
+        }
+
+        public void Die()
+        {
+            health = 0;
+            _isDying = true;
+            charController.enabled = playerMovement.enabled = playerInteraction.enabled = false;
+            _respawnAction.performed += OnRespawn;
+        }
+
+        private void PlayDeathAnimation(float rotationSpeed)
+        {
+            Quaternion currentRot = transform.rotation;
+            Quaternion targetRot = currentRot * Quaternion.AngleAxis(90f, new Vector3(-90f, currentRot.y, 0f));
+            transform.rotation = Quaternion.RotateTowards(currentRot, targetRot, Time.deltaTime * rotationSpeed);
+        }
+        
+        private void Respawn()
+        {
+            _isDying = false;
+            health = maxHealth;
+            transform.position = spawnPoint.position;
+            transform.rotation = Quaternion.identity;
+            
+            charController.enabled = playerMovement.enabled = playerInteraction.enabled = true;
+        }
+    }
+}
