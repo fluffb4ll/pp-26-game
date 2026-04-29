86a662c240f6afc94c51e971f853a571e2d879e0
ladmiss <swift6423@gmail.com>
2026-04-29 21:48:18 +0500

    Restore daily rewards sdk time

--- Assets/Scripts/SDK/DailyRewards.cs
+++ Assets/Scripts/SDK/DailyRewards.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Reflection;
 using YG;
 
 namespace SDK
@@ -9,43 +7,18 @@ namespace SDK
     /// </summary>
     public class DailyRewards
     {
-        private static bool _serverTimeMethodCached;
-        private static MethodInfo _serverTimeMethod;
-
-        public static long GetServerTime
-        {
-            get
-            {
-                var serverTimeMethod = GetServerTimeMethod();
-                return serverTimeMethod is null
-                    ? DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()
-                    : Convert.ToInt64(serverTimeMethod.Invoke(null, null));
-            }
-        }
+        public static long GetServerTime => YG2.ServerTime();
         
         /// <summary>
         /// Выдаёт награду за вход, если прошли сутки с момента получения последней награды
         /// </summary>
         public static void GiveDailyReward()
         {
-            var serverTime = GetServerTime;
-
-            if (serverTime - YG2.saves.lastDailyRewardTime < 24 * 60 * 60 * 1000)
+            if (GetServerTime - YG2.saves.lastDailyRewardTime < 24 * 60 * 60 * 1000)
                 return;
             
             YG2.saves.coins++;  // плейсхолдер - в дальнейшем стоит поменять (например, на "календарь" наград)
-            YG2.saves.lastDailyRewardTime = serverTime;
-            YG2.SaveProgress();
-        }
-
-        private static MethodInfo GetServerTimeMethod()
-        {
-            if (_serverTimeMethodCached)
-                return _serverTimeMethod;
-
-            _serverTimeMethodCached = true;
-            _serverTimeMethod = typeof(YG2).GetMethod("ServerTime", BindingFlags.Public | BindingFlags.Static);
-            return _serverTimeMethod;
+            YG2.saves.lastDailyRewardTime = GetServerTime;
         }
     }
 }
