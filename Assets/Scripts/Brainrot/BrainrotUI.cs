97434ba76afbde6ce6b1d020f3ac39a10c62517e
Ivan Antropov <real.fluffball@gmail.com>
2026-05-17 14:00:35 +0500

    Removed redundant code

--- Assets/Scripts/Brainrot/BrainrotUI.cs
+++ Assets/Scripts/Brainrot/BrainrotUI.cs
@@ -1,5 +1,4 @@
 using UI;
-using UnityEngine;
 
 namespace Brainrot
 {
@@ -7,31 +6,5 @@ namespace Brainrot
     /// <summary>
     /// Управляет элементами интерфейса, связанными с брейнротом
     /// </summary>
-    public class BrainrotUI : InfoUI
-    {
-        protected override void Awake()
-        {
-            base.Awake();
-        }
-
-        protected override void Start()
-        {
-            base.Start();
-        }
-
-        protected override void Update()
-        {
-            base.Update();
-        }
-
-        protected override void OnEnable()
-        {
-            base.OnEnable();
-        }
-
-        protected override void OnDisable()
-        {
-            base.OnDisable();
-        }
-    }
+    public class BrainrotUI : InfoUI { }
 }
